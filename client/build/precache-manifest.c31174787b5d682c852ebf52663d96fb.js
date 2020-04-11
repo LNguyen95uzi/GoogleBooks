5b700d89202511e6c1454a79caa401455e19b104
Long Nguyen <Long.Nguyen@temple.edu>
2020-04-11 19:48:43 -0400

    gh-pages

---
+++ client/build/precache-manifest.c31174787b5d682c852ebf52663d96fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e795bd6cd404fae8a0f",
+    "url": "/static/js/main.eaf8ad8d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7cd0864745360fc4bb53",
+    "url": "/static/css/2.2ebcb072.chunk.css"
+  },
+  {
+    "revision": "7cd0864745360fc4bb53",
+    "url": "/static/js/2.f9f5dc27.chunk.js"
+  },
+  {
+    "revision": "26b40218d310f76e3d3239bb93b068c7",
+    "url": "/index.html"
+  }
+];
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Books from "./components/Books";
-import Search from "./components/Search";
-import Navbar from "./components/Navbar";
+import Books from "./Components/Books";
+import Search from "./Components/Search";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div>
         <Navbar />
         <Switch>
-          <Route path="/search" component={Search} exact></Route>
+          <Route path="/search" component={Books} exact></Route>
           <Route path="/saved" component={Books} exact></Route>
         </Switch>
       </div>
